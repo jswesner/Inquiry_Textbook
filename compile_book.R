fd5af23e3feac51d7b94278b7ad1152d04eb09e1
Jeff Wesner <jeff.wesner@usd.edu>
2020-11-18 12:55:21 -0700

    add title and authors

--- compile_book.R
+++ compile_book.R
@@ -1,4 +1,7 @@
 library(bookdown)
 
-render_book("index.Rmd", output_format = "bookdown::gitbook")
+file.remove("_main.Rmd")
+
+render_book("index.Rmd", output_format = "bookdown::gitbook",
+            output_dir = "/docs")
 
