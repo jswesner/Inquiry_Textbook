fddfbc1013126ba4239ec9d4e4a411faec87feb8
Jeff Wesner <jeff.wesner@usd.edu>
2021-01-07 16:03:35 -0600

    update

--- compile_book.R
+++ compile_book.R
@@ -3,8 +3,8 @@ library(here)
 
 # file.remove("_main.Rmd")
 
-render_book("index.Rmd", output_format = "bookdown::gitbook",
-            output_dir = here("/docs"))
+# render_book("index.Rmd", output_format = "bookdown::gitbook",
+#             output_dir = here("/docs"))
 
-bookdown::publish_book()
-# bookdown::publish_book(render = 'local')
+# bookdown::publish_book()
+bookdown::publish_book(render = 'local')
